0d54d6fe980e61c8246c4e4a404b797ac47da6cb
JonMooseMejia <48665042+JonMooseMejia@users.noreply.github.com>
2020-12-09 21:14:36 -0600

    Add files via upload

---
+++ examples/javascript/form.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function () {
+    $("#submitboton").click(function (e) {
+        $(".body").css("filter","blur(1px)" );
+        $(".initiallyHidden").css("visibility", "visible");
+     });
+});
+
